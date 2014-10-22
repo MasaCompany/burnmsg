9fe454c4426fc80a57444ff7c1ee2bdff551a629
Ed Zynda <ezynda3@gmail.com>
2014-10-22 16:43:37 -0700

    Fixed typo

--- app/views/messages/store.blade.php
+++ app/views/messages/store.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="alert alert-success">
     <strong>Attention! The following URL can only be viewed ONCE! After that the message is destroyed FOREVER!</strong>
-    Here is your message URL: {{ $url }}
+    <p>Here is the URL {{ secure_url('/' . $url . '/' . $key) }}</p>
 </div>
 @stop
